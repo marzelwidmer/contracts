402971fbb3e26f5bd00924ee50e39d8c61afd00e
Marcel Widmer <marzelwidmer@gmail.com>
2020-09-14 08:59:49 +0200

    Change Persons Contracts

--- META-INF/ch.keepcalm.demo/kboot-puername/0.0.0/contracts/PersonRestContractTest.groovy
+++ META-INF/ch.keepcalm.demo/kboot-puername/0.0.0/contracts/PersonRestContractTest.groovy
@@ -3,7 +3,7 @@ import org.springframework.cloud.contract.spec.Contract
 Contract.make {
     request {
         method 'GET'
-        url '/api/partners'
+        url '/api/persons'
         headers {
             header(accept(), $(consumer(regex("application/(hal\\+)?json")), producer("application/hal+json")))
         }
