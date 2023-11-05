7fe1cb01011f9f9af0de2778054b39f745595baa
c3smonkey <c3smonkey@gmail.com>
2023-11-05 09:02:35 +0100

    chore: add kotlin contracts

--- META-INF/ch.keepcalm/application-module/0.0.0/partner/3_shouldReturnOnePartner.kt
+++ META-INF/ch.keepcalm/application-module/0.0.0/partner/3_shouldReturnOnePartner.kt
@@ -1,1 +1,28 @@
-class  Foo()
+package contracts
+
+import org.springframework.cloud.contract.spec.Contract
+
+val contracts = Contract.make {
+    description = "Should return info about other endpoints to query"
+    request {
+        method = Contract.HttpMethods.HttpMethod.GET
+        urlPath = value("/partners/{id}")
+        urlParameters {
+            parameter("id", "1")
+        }
+    }
+    response {
+        status = 200
+        headers {
+            header("Content-Type", "application/json")
+        }
+        body("""
+            {
+                "name" : "John"
+            }
+        """)
+        matchers {
+            header("Content-Type", "application/json.*")
+        }
+    }
+}
